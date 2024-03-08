0f827392efad1f2614eacdc7c715f90788a6120f
Kasinan Rordthab <sameyu.ping@gmail.com>
2024-03-08 21:02:03 +0700

    add list coin page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
+import {Button} from 'react-native';
 import ListCoin from './src/screens/ListCoin';
 import MyCoin from './src/screens/MyCoin';
 
@@ -10,8 +11,32 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="listCoin">
-        <Stack.Screen name="listCoin" component={ListCoin} />
-        <Stack.Screen name="myCoin" component={MyCoin} />
+        <Stack.Screen
+          name="listCoin"
+          component={ListCoin}
+          options={({navigation}) => ({
+            title: 'List Coin',
+            headerStyle: {
+              backgroundColor: '#2E2D3D',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            headerRight: () => (
+              <Button
+                onPress={() => navigation.navigate('myCoin')}
+                title="My Coin"
+                color="#fff"
+              />
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="myCoin"
+          component={MyCoin}
+          options={{title: 'My Coin'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
