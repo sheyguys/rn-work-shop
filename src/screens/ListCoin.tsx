e9e7cf08765fd77dc96736cc74e6dd2a870a660f
Kasinan Rordthab <sameyu.ping@gmail.com>
2024-03-08 19:08:26 +0700

    initial project

---
+++ src/screens/ListCoin.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ListCoin = () => {
+  return (
+    <View>
+      <Text>ListCoin</Text>
+    </View>
+  );
+};
+
+export default ListCoin;
---
+++ src/screens/MyCoin.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const MyCoin = () => {
+  return (
+    <View>
+      <Text>MyCoin</Text>
+    </View>
+  );
+};
+
+export default MyCoin;
